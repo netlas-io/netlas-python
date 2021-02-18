22f6a34146d2ef9cddaf3537dabc7bd9d8535fca
Босов Юрий Олегович <uob@acribia.ru>
2021-02-18 15:33:21 +0000

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DEPENDENCIES = open("requirements.txt", "r").read().split("\n")
 
 setup(
     name="netlas",
-    version="0.0.1-dev",
+    version="0.0.2-dev",
     author="Netlas.IO",
     author_email="support@netlas.io",
     description="Netlas.IO API package",
