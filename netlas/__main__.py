c704fc5aa128b968312479a223e9a016585a92e2
uob <uob@acribia.ru>
2021-04-06 15:35:57 +0100

    Added host option

--- netlas/__main__.py
+++ netlas/__main__.py
@@ -2,7 +2,6 @@ import netlas
 import click
 import json
 
-
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 # Default entry point for CLI
@@ -18,8 +17,8 @@ def main():
     "-d",
     "--datatype",
     help="Query data type",
-    type=click.Choice(["URI", "CRT", "DOM"], case_sensitive=False),
-    default="URI",
+    type=click.Choice(["uri", "cert", "domain"], case_sensitive=False),
+    default="uri",
     show_default=True,
 )
 @click.option(
@@ -30,18 +29,25 @@ def main():
     prompt=True,
     hide_input=True,
 )
-@click.option("-p", "--pretty", help="Pretty print output", is_flag=True)
+@click.option("-f",
+              "--format",
+              help="Output format",
+              default="yaml",
+              type=click.Choice(['json', 'yaml'], case_sensitive=False),
+              show_default=True)
 @click.argument("querystring")
-def query(datatype, apikey, querystring, pretty):
-    if datatype == "URI":
-        ns_con = netlas.Netlas(api_key=apikey)
-        query_res = ns_con.query(query=querystring)
-        if pretty:
-            print(json.dumps(query_res, indent=4))
-        else:
-            print(query_res)
-    else:
-        print("Sorry, this type is not supported yet")
+@click.option("-s",
+              "--server",
+              help="Netlas API server",
+              default="https://app.netlas.io",
+              show_default=True)
+def query(datatype, apikey, format, querystring, server):
+    """Search query."""
+    ns_con = netlas.Netlas(api_key=apikey, apibase=server)
+    query_res = ns_con.query(query=querystring,
+                             output_format=format,
+                             datatype=datatype)
+    print(query_res)
 
 
 @main.command()
@@ -49,8 +55,8 @@ def query(datatype, apikey, querystring, pretty):
     "-d",
     "--datatype",
     help="Query data type",
-    type=click.Choice(["URI", "CRT", "DOM"], case_sensitive=False),
-    default="URI",
+    type=click.Choice(["uri", "cert", "domain"], case_sensitive=False),
+    default="uri",
     show_default=True,
 )
 @click.option(
@@ -61,28 +67,83 @@ def query(datatype, apikey, querystring, pretty):
     prompt=True,
     hide_input=True,
 )
-@click.option("-p", "--pretty", help="Pretty print output", is_flag=True)
+@click.option("-f",
+              "--format",
+              help="Output format",
+              default="yaml",
+              type=click.Choice(['json', 'yaml'], case_sensitive=False),
+              show_default=True)
 @click.argument("querystring")
-def count(datatype, apikey, querystring, pretty):
+@click.option("-s",
+              "--server",
+              help="Netlas API server",
+              default="https://app.netlas.io",
+              show_default=True)
+def count(datatype, apikey, querystring, server):
     """Calculate count of query results."""
-    if datatype == "URI":
-        ns_con = netlas.Netlas(api_key=apikey)
-        query_res = ns_con.count(query=querystring)
-        if pretty:
-            print(json.dumps(query_res, indent=4))
-        else:
-            print(query_res)
-    else:
-        print("Sorry, this type is not supported yet")
+    ns_con = netlas.Netlas(api_key=apikey, apibase=server)
+    query_res = ns_con.count(query=querystring,
+                             output_format=format,
+                             datatype=datatype)
+    print(query_res)
 
 
 @main.command()
 @click.option(
-    "-d",
-    "--datatype",
+    "-a",
+    "--apikey",
+    help="User API key",
+    required=True,
+    prompt=True,
+    hide_input=True,
+)
+@click.option("-f",
+              "--format",
+              help="Output format",
+              default="yaml",
+              type=click.Choice(['json', 'yaml'], case_sensitive=False),
+              show_default=True)
+@click.argument("querystring")
+@click.option("-s",
+              "--server",
+              help="Netlas API server",
+              default="https://app.netlas.io",
+              show_default=True)
+def stat(apikey, querystring, server):
+    """Get statistics for query."""
+    ns_con = netlas.Netlas(api_key=apikey, apibase=server)
+    query_res = ns_con.stat(query=querystring, output_format=format)
+    print(query_res)
+
+
+@main.command()
+@click.option(
+    "-a",
+    "--apikey",
+    help="User API key",
+    required=True,
+    prompt=True,
+    hide_input=True,
+)
+@click.option("-s",
+              "--server",
+              help="Netlas API server",
+              default="https://app.netlas.io",
+              show_default=True)
+def profile(apikey, server):
+    """Get user profile data."""
+    ns_con = netlas.Netlas(api_key=apikey, apibase=server)
+    profile = ns_con.profile()
+    print(profile)
+
+
+@main.command()
+@click.option(
+    "-t",
+    "--hosttype",
     help="Query data type",
-    type=click.Choice(["URI", "CRT", "DOM"], case_sensitive=False),
-    default="URI",
+    type=click.Choice(["ip", "domain"], case_sensitive=False),
+    default="ip",
     show_default=True,
 )
 @click.option(
@@ -93,18 +154,23 @@ def count(datatype, apikey, querystring, pretty):
     prompt=True,
     hide_input=True,
 )
-@click.option("-p", "--pretty", help="Pretty print output", is_flag=True)
-@click.argument("querystring")
-def stat(datatype, apikey, querystring, pretty):
-    if datatype == "URI":
-        ns_con = netlas.Netlas(api_key=apikey)
-        query_res = ns_con.stat(query=querystring)
-        if pretty:
-            print(json.dumps(query_res, indent=4))
-        else:
-            print(query_res)
-    else:
-        print("Sorry, this type is not supported yet")
+@click.option("-f",
+              "--format",
+              help="Output format",
+              default="yaml",
+              type=click.Choice(['json', 'yaml'], case_sensitive=False),
+              show_default=True)
+@click.argument("host")
+@click.option("-s",
+              "--server",
+              help="Netlas API server",
+              default="https://app.netlas.io",
+              show_default=True)
+def host(hosttype, apikey, format, host, server):
+    """Search query."""
+    ns_con = netlas.Netlas(api_key=apikey, apibase=server)
+    query_res = ns_con.host(host=host, output_format=format, hosttype=hosttype)
+    print(query_res)
 
 
 if __name__ == "__main__":
--- netlas/client.py
+++ netlas/client.py
@@ -1,49 +1,114 @@
 import requests
 import json
+import yaml
 
 
 class Netlas:
     debug: bool = False
 
-    def __init__(self, api_key: str,
-                 apibase="https://app.netlas.io") -> None:
+    def __init__(self, api_key: str, apibase="https://app.netlas.io") -> None:
         self.api_key: str = api_key
         self.apibase: str = apibase
 
-    def _request(self, query: str, endpoint: str) -> dict:
+    def _request(self, endpoint: str = "/api/", params: object = {}) -> dict:
         ret: dict = {}
-        params = {'q': query, 'api_key': self.api_key}
         try:
-            r = requests.get(
-                f"{self.apibase}{endpoint}", params=params)
+            r = requests.get(f"{self.apibase}{endpoint}",
+                             params=params,
+                             verify=False)
             response_data = json.loads(r.text)
         except json.JSONDecodeError:
-            ret['error'] = "Failed to decode response data to JSON"
+            ret["error"] = "Failed to decode response data to JSON"
             if self.debug:
-                ret['error_description'] = r.reason
-                ret['error_data'] = r.text
+                ret["error_description"] = r.reason
+                ret["error_data"] = r.text
+            return ret
         except requests.HTTPError:
-            ret['error'] = "HTTP error"
+            ret["error"] = "HTTP error"
             if self.debug:
-                ret['error_description'] = r.reason
-                ret['error_data'] = r.text
+                ret["error_description"] = r.reason
+                ret["error_data"] = r.text
+            return ret
         if r.status_code == 200:
-            ret['data'] = response_data
+            ret["data"] = response_data
         else:
-            ret['error'] = "Non 200 response code"
+            ret["error"] = "Non 200 response code"
             if self.debug:
-                ret['error_description'] = r.reason
-                ret['error_data'] = r.text
+                ret["error_description"] = r.reason
+                ret["error_data"] = r.text
         return ret
 
-    def query(self, query: str) -> dict:
-        ret = self._request(query=query, endpoint="/api/search/")
-        return ret
+    def _return(self, data, format: str = "json"):
+        if format == "json":
+            return data
+        elif format == "yaml":
+            return yaml.safe_dump(data)
+        else:
+            return "Unknown output format"
 
-    def count(self, query: str) -> dict:
-        ret = self._request(query=query, endpoint="/api/count/")
-        return ret
+    def query(self,
+              query: str,
+              datatype: str = "uri",
+              output_format: str = "json") -> dict:
+        endpoint = "/api/uri/"
+        if datatype == "cert":
+            endpoint = "/api/certs/"
+        elif datatype == "domain":
+            endpoint = "/api/domains/"
+        ret = self._request(
+            endpoint=endpoint,
+            params={
+                "q": query,
+                "api_key": self.api_key
+            },
+        )
+        return self._return(ret, format=output_format)
 
-    def stat(self, query: str) -> dict:
-        ret = self._request(query=query, endpoint="/api/stat/")
-        return ret
+    def count(self,
+              query: str,
+              datatype: str = "uri",
+              output_format: str = "json") -> dict:
+        endpoint = "/api/uri_count/"
+        if datatype == "cert":
+            endpoint = "/api/certs_count/"
+        elif datatype == "domain":
+            endpoint = "/api/domains_count/"
+        ret = self._request(
+            endpoint=endpoint,
+            params={
+                "q": query,
+                "api_key": self.api_key
+            },
+        )
+        return self._return(ret, format=output_format)
+
+    def stat(self, query: str, output_format: str = "json") -> dict:
+        ret = self._request(
+            endpoint="/api/uri_stat/",
+            params={
+                "q": query,
+                "api_key": self.api_key
+            },
+        )
+        return self._return(ret, format=output_format)
+
+    def profile(self, output_format: str = "json") -> dict:
+        ret = self._request(endpoint="/api/profile/",
+                            params={"api_key": self.api_key})
+        return self._return(ret, format=output_format)
+
+    def host(self,
+             host: str,
+             hosttype: str = "ip",
+             output_format: str = "json") -> dict:
+        endpoint = "/api/ip/"
+        if hosttype == "domain":
+            endpoint = "/api/domain/"
+        ret = self._request(
+            endpoint=endpoint,
+            params={
+                "q": host,
+                "api_key": self.api_key
+            },
+        )
+        return self._return(ret, format=output_format)
