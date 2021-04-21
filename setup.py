a8a0c4f08a56d52805b36fad5ae79d2ee7c023ee
uob <uob@acribia.ru>
2021-04-21 11:10:45 +0000

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DEPENDENCIES = open("requirements.txt", "r").read().split("\n")
 
 setup(
     name="netlas",
-    version="0.0.10",
+    version="0.0.11",
     author="Netlas.IO",
     author_email="support@netlas.io",
     description="Netlas.IO API package",
