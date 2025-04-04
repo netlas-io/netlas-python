76fc266641860d6a51a2c7d3b4c6448ba4701af4
Arthur Kotylevskiy <kotylevskiy@gmail.com>
2025-04-04 10:14:22 +0300

    ThrottlingError: docstrings and import via __init__.py added

--- netlas/__init__.py
+++ netlas/__init__.py
@@ -1,2 +1,3 @@
 from netlas.client import Netlas
 from netlas.exception import APIError
+from netlas.exception import ThrottlingError
--- netlas/exception.py
+++ netlas/exception.py
@@ -1,5 +1,6 @@
 class APIError(Exception):
     """Basic Netlas.io Exception class"""
+    
     def __init__(self, value, description={}):
         self.value = value
         self.description: dict = description
@@ -9,8 +10,22 @@ class APIError(Exception):
 
 
 class ThrottlingError(Exception):
-    def __init__(self, retry_after):
+    """
+    Exception raised when a throttling limit is encountered.
+
+    Attributes:
+        retry_after (int): The amount of time (in seconds) to wait before retrying.
+    """
+
+    def __init__(self, retry_after: int):
+        """
+        Initialize the ThrottlingError.
+
+        Args:
+            retry_after (int): The time to wait before retrying, in seconds.
+        """
         self.retry_after = retry_after
 
     def __str__(self):
-        return self.retry_after
+        """Return the retry time in seconds as a string."""
+        return str(self.retry_after)
